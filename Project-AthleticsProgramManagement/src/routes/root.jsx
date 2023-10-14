b49b384777c6389f6585aecc34ecae1bf24ee927
Weerachat <Weerachat.kdw@kkumail.com>
2023-10-14 14:52:51 +0700

    [Update] Dropdown

--- Project-AthleticsProgramManagement/src/routes/root.jsx
+++ Project-AthleticsProgramManagement/src/routes/root.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './root.css'
 import { useSelector } from 'react-redux';
+import UserDropdown from '../components/userDropDown/index';
+import Filter from '../components/competitionPage/componants/filter';
+
 
 
 function LineHighlight() {
@@ -11,62 +14,89 @@ function LineHighlight() {
   </div>)
 }
 
-function Profile({ profileData }) {
+function ArrowUp() {
   return (
-    <div className='Profile flex flex-row items-center justify-end w-full bg-while mx-6'>
-      <svg width="19" height="25" viewBox="0 0 19 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M18.5462 15.24L17.3296 13.2204C17.0741 12.7702 16.8429 11.9186 16.8429 11.4198V8.3417C16.8429 5.48263 15.164 3.01288 12.7429 1.85708C12.1103 0.737788 10.9423 0.0443115 9.60401 0.0443115C8.27789 0.0443115 7.08564 0.762121 6.453 1.89358C4.08058 3.07371 2.43813 5.51913 2.43813 8.3417V11.4198C2.43813 11.9186 2.20697 12.7702 1.95148 13.2082L0.722691 15.24C0.236041 16.0551 0.126545 16.9554 0.430701 17.7827C0.722691 18.5979 1.41617 19.2305 2.31647 19.5347C4.67672 20.3376 7.15864 20.7269 9.64051 20.7269C12.1224 20.7269 14.6043 20.3376 16.9646 19.5468C17.8162 19.267 18.4732 18.6222 18.7895 17.7827C19.1059 16.9432 19.0207 16.0186 18.5462 15.24Z" fill="#5B72FF" />
-        <path d="M13.0589 21.9558C12.548 23.3671 11.1975 24.3769 9.61589 24.3769C8.65476 24.3769 7.70579 23.9875 7.03664 23.2941C6.64732 22.9291 6.35533 22.4424 6.185 21.9436C6.34316 21.9679 6.50132 21.9801 6.67165 22.0044C6.95147 22.0409 7.24347 22.0774 7.53546 22.1018C8.22894 22.1626 8.93458 22.1991 9.64023 22.1991C10.3337 22.1991 11.0272 22.1626 11.7085 22.1018C11.964 22.0774 12.2195 22.0653 12.4628 22.0288C12.6575 22.0044 12.8521 21.9801 13.0589 21.9558Z" fill="#5B72FF" />
-      </svg>
+    <svg width="15" className=' mx-6 ' height="9" viewBox="0 0 24 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M13.0607 1.34852C12.4749 0.762733 11.5251 0.762733 10.9393 1.34852L1.3934 10.8945C0.807611 11.4802 0.807611 12.43 1.3934 13.0158C1.97919 13.6016 2.92893 13.6016 3.51472 13.0158L12 4.5305L20.4853 13.0158C21.0711 13.6016 22.0208 13.6016 22.6066 13.0158C23.1924 12.43 23.1924 11.4802 22.6066 10.8945L13.0607 1.34852ZM13.5 3.966V2.40918H10.5V3.966H13.5Z" fill="white" />
+    </svg>
+  )
+}
 
-      <div class="Image w-10 h-10 mx-6 bg-white rounded-full ">
-        <img  class='object-cover rounded-full' src={profileData.imageUrl} alt={profileData.name} />
+function ArrowDown() {
+  return (
+    <svg width="15" className=' mx-6 ' height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg" >
+      <path d="M6.89879 8.53466C7.28931 8.92519 7.92248 8.92519 8.313 8.53466L14.677 2.1707C15.0675 1.78018 15.0675 1.14701 14.677 0.756489C14.2864 0.365965 13.6533 0.365965 13.2628 0.756489L7.6059 6.41334L1.94904 0.756489C1.55852 0.365965 0.925353 0.365965 0.534828 0.756489C0.144304 1.14701 0.144304 1.78018 0.534828 2.1707L6.89879 8.53466ZM6.6059 7.16992V7.82756H8.6059V7.16992H6.6059Z" fill="white" />
+    </svg>
+  )
+}
+
+function Profile({ profileData, onDropdownClick, DropdownOpen }) {
+  return (
+    <>
+      <div className='Profile flex flex-row items-center justify-end bg-while mx-6' onClick={onDropdownClick}>
+        <svg width="19" height="25" viewBox="0 0 19 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18.5462 15.24L17.3296 13.2204C17.0741 12.7702 16.8429 11.9186 16.8429 11.4198V8.3417C16.8429 5.48263 15.164 3.01288 12.7429 1.85708C12.1103 0.737788 10.9423 0.0443115 9.60401 0.0443115C8.27789 0.0443115 7.08564 0.762121 6.453 1.89358C4.08058 3.07371 2.43813 5.51913 2.43813 8.3417V11.4198C2.43813 11.9186 2.20697 12.7702 1.95148 13.2082L0.722691 15.24C0.236041 16.0551 0.126545 16.9554 0.430701 17.7827C0.722691 18.5979 1.41617 19.2305 2.31647 19.5347C4.67672 20.3376 7.15864 20.7269 9.64051 20.7269C12.1224 20.7269 14.6043 20.3376 16.9646 19.5468C17.8162 19.267 18.4732 18.6222 18.7895 17.7827C19.1059 16.9432 19.0207 16.0186 18.5462 15.24Z" fill="#5B72FF" />
+          <path d="M13.0589 21.9558C12.548 23.3671 11.1975 24.3769 9.61589 24.3769C8.65476 24.3769 7.70579 23.9875 7.03664 23.2941C6.64732 22.9291 6.35533 22.4424 6.185 21.9436C6.34316 21.9679 6.50132 21.9801 6.67165 22.0044C6.95147 22.0409 7.24347 22.0774 7.53546 22.1018C8.22894 22.1626 8.93458 22.1991 9.64023 22.1991C10.3337 22.1991 11.0272 22.1626 11.7085 22.1018C11.964 22.0774 12.2195 22.0653 12.4628 22.0288C12.6575 22.0044 12.8521 21.9801 13.0589 21.9558Z" fill="#5B72FF" />
+        </svg>
+
+        <div class="Image w-10 h-10 mx-6 bg-white rounded-full ">
+          <img class='object-cover rounded-full' src={profileData.imageUrl} alt={profileData.name} />
+        </div>
+
+        <label className=' text-white ' >{profileData.name}</label>
+        {DropdownOpen ? <ArrowUp/> : <ArrowDown />}
+        {/* <UserDropdown /> */}
       </div>
+    </>
 
-      <label className=' text-white ' >{profileData.name}</label>
-      <svg width="15" className=' mx-6 ' height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6.89879 8.53466C7.28931 8.92519 7.92248 8.92519 8.313 8.53466L14.677 2.1707C15.0675 1.78018 15.0675 1.14701 14.677 0.756489C14.2864 0.365965 13.6533 0.365965 13.2628 0.756489L7.6059 6.41334L1.94904 0.756489C1.55852 0.365965 0.925353 0.365965 0.534828 0.756489C0.144304 1.14701 0.144304 1.78018 0.534828 2.1707L6.89879 8.53466ZM6.6059 7.16992V7.82756H8.6059V7.16992H6.6059Z" fill="white" />
-      </svg>
-
-
-
-    </div>
   )
 }
 
 function NoLogin() {
   return (
-    <Link to="/login" className='NoLogin w-full flex items-center justify-end mr-14'>
-      <div className='rounded-full bg-[#004CEE] flex items-center justify-center font-inter px-6 p-1'>
-        <label class="text-center font-inter text-base font-normal text-[#fff] ">
-          Login/ Sign up
-        </label>
-      </div>
-    </Link>
+    <div className='NoLogin w-full flex items-center justify-end mr-14'>
+      < Link to="/login">
+        <div className='rounded-full bg-[#004CEE] flex items-center justify-center font-inter px-6 p-1'>
+          <label class="text-center font-inter text-base font-normal text-[#fff] ">
+            Login/ Sign up
+          </label>
+        </div>
+      </Link>
+    </div>
   )
 }
 
 function Root() {
   const profile = useSelector(state => state.profile);
   const display = useSelector(state => state.display);
+  const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
+  const handleDropdownClick = () => {
+    setIsUserDropdownOpen(!isUserDropdownOpen);
+  };
   return (
-    <nav className='Navbar h-14 bg-[#002880] sticky top-0 px-[24px] flex flex-row z-10 '>
-      <Link to="/">
-        <img className=' absolute h-20 ' src="https://static.vecteezy.com/system/resources/thumbnails/023/654/784/small/golden-logo-template-free-png.png"
-          alt="" />
-      </Link>
-      <div className='Menu pt-[12px] flex flex-row items-end pl-[100px] font-inter font-light text-[#fff]'>
-        <Link to="/" className={`Home MenuBox flex flex-col  ${display === 'home' ? 'font-semibold  bg-menu-blue justify-end' : ''}`}>
-          Home
-          {display === 'home' && <LineHighlight />}
+    <>
+      <nav className='Navbar h-14 bg-[#002880] sticky top-0 px-[24px] flex flex-row z-10 '>
+        <Link to="/">
+          <img className=' absolute h-20 ' src="https://static.vecteezy.com/system/resources/thumbnails/023/654/784/small/golden-logo-template-free-png.png"
+            alt="" />
         </Link>
-        <Link to="/competition" className={`Competition MenuBox flex flex-col  ${display === 'competition' ? 'font-semibold bg-menu-blue justify-end' : ''}`}>
-          Competition
-          {display === 'competition' && <LineHighlight />}
-        </Link>
-      </div>
-      {profile ? <Profile profileData={profile}/> : <NoLogin />}
-    </nav>
+        <div className=' flex flex-row justify-between w-full'>
+          <div className='Menu pt-[12px] flex flex-row items-end pl-[100px] font-inter font-light text-[#fff]'>
+            <Link to="/" className={`Home MenuBox flex flex-col  ${display === 'home' ? 'font-semibold  bg-menu-blue justify-end' : ''}`}>
+              Home
+              {display === 'home' && <LineHighlight />}
+            </Link>
+            <Link to="/competition" className={`Competition MenuBox flex flex-col  ${display === 'competition' ? 'font-semibold bg-menu-blue justify-end' : ''}`}>
+              Competition
+              {display === 'competition' && <LineHighlight />}
+            </Link>
+          </div>
+          {profile ? <Profile profileData={profile} onDropdownClick={handleDropdownClick} DropdownOpen={isUserDropdownOpen}/> : <NoLogin />}
+        </div>
+      </nav>
+      {isUserDropdownOpen && <UserDropdown />}
+      {display === 'competition' && <Filter />}
+    </>
   );
 }
 
