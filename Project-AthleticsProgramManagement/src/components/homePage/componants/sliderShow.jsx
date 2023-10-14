e93af171b00bf8a54a12a402ac47b8e871c969ae
Weerasak001 <weerasak_wts@kkumail.com>
2023-10-15 01:18:04 +0700

    home page

--- Project-AthleticsProgramManagement/src/components/homePage/componants/sliderShow.jsx
+++ Project-AthleticsProgramManagement/src/components/homePage/componants/sliderShow.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './slider.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./slider.css";
 
 const apiKey = "6afd80ac0cb843d1974f525398eb4145";
 const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=${apiKey}`;
-const delay = 2500;
+const delay = 3000;
 
 function SlideShow() {
   const [newsData, setNewsData] = useState([]);
@@ -16,7 +16,7 @@ function SlideShow() {
       const data = await response.json();
       setNewsData(data.articles.slice(0, 4)); // Get the first 4 articles only
     } catch (error) {
-      console.error('Error fetching news:', error);
+      console.error("Error fetching news:", error);
     }
   }
 
@@ -27,7 +27,10 @@ function SlideShow() {
   useEffect(() => {
     resetTimeout();
     timeoutRef.current = setTimeout(
-      () => setIndex((prevIndex) => (prevIndex === newsData.length - 1 ? 0 : prevIndex + 1)),
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === newsData.length - 1 ? 0 : prevIndex + 1
+        ),
       delay
     );
 
@@ -51,49 +54,57 @@ function SlideShow() {
       >
         {newsData.map((article, idx) => (
           <div className="inline-block H-news w-full" key={idx}>
-             {/* <div className=' relative'>
+            <div className=" relative">
+              <div >
               <div className='Shadows h-full w-full absolute truncate' />
-              <div className="w-[63%] absolute text-5xl text-white font-inter font-bold 
-              leading-normal  bottom-[240px] left-[77px]  truncate ">
-                {hText}
-              </div>
-              <button className="flex w-[251px] h-[76px] mb-[9%] px-4 justify-center items-center space-x-1 flex-shrink-0 
-              rounded-lg border border-white absolute bottom-0 left-[192px]">
-                <div class="text-[#FC563D] font-inter text-[30px] font-semibold leading-normal">
-                  Read more . . .
+                <div
+                  className="Title w-[60%] absolute text-5xl text-white font-inter font-bold 
+              leading-normal  bottom-[240px] left-[77px]  truncate "
+                >
+                  {article.title}
                 </div>
-              </button>
-              <div className='ING bg-gray h-screen-4rem w-full'>
-                <img className='inline-block h-full w-full object-cover'
-                  src='https://img.olympicchannel.com/images/image/private/t_16-9_640/f_auto/v1538355600/primary/ntg4t1lytzt11bzbnkqv'
-                  // src='https://cdn2.vectorstock.com/i/1000x1000/14/06/running-athletes-sport-and-competition-background-vector-22681406.jpg'
-                  alt="People running to the finish line" />
-              </div>
+                <a tget="_blank" href={article.url}>
+                  <button
+                    className="flex w-[251px] h-[76px] mb-[9%] px-4 justify-center items-center space-x-1 flex-shrink-0 
+              rounded-lg border border-white absolute bottom-[0px] left-[192px]"
+                  >
+                    <div class="text-[#FC563D] font-inter text-[30px] font-semibold leading-normal">
+                      Read more . . .
+                    </div>
+                  </button>
+                </a>
 
-            </div> */}
-              <img className='inline-block h-full w-full object-cover'
-              src={article.urlToImage}
-              alt={article.title} />
-            {/* ... */}
+                <img
+                  className="inline-block h-screen-nav w-full object-cover overflow-hidden Shadow"
+                  src={article.urlToImage}
+                  alt={article.title}
+                />
+              </div>
+            </div>
           </div>
         ))}
       </div>
-      {/* ... (ตามด้วยส่วนอื่นๆของโค้ดของคุณ) */}
+             <div className="Tap flex justify-center space-x-4 absolute bottom-2 left-1/2 transform -translate-x-1/2 py-2 ">
+         {newsData.map((_, idx) => (
+           <div
+             key={idx}
+            className={`h-3 w-20 rounded-full cursor-pointer  ${index === idx ? "bg-[#FB3D21]" : " bg-[#707070]"}`}
+             onClick={() => setIndex(idx)}
+           ></div>
+         ))}
+       </div>
     </div>
+
   );
 }
 
 export default SlideShow;
 
-
-
-
-
 // import React, { useState, useEffect, useRef } from 'react';
 // import './slider.css'
 
 // const news = [News1, News2, News3, News4];
-// // 
+// //
 // let hText = "USA take over number two in World Ranking";
 // const colors = ["#0088FE", "#00C49F", "#FFBB28", "#00C49F"];
 // const delay = 2500;
@@ -133,11 +144,11 @@ export default SlideShow;
 //           >
 //             <div className=' relative'>
 //               <div className='Shadows h-full w-full absolute truncate' />
-//               <div className="w-[63%] absolute text-5xl text-white font-inter font-bold 
+//               <div className="w-[63%] absolute text-5xl text-white font-inter font-bold
 //               leading-normal  bottom-[240px] left-[77px]  truncate ">
 //                 {hText}
 //               </div>
-//               <button className="flex w-[251px] h-[76px] mb-[9%] px-4 justify-center items-center space-x-1 flex-shrink-0 
+//               <button className="flex w-[251px] h-[76px] mb-[9%] px-4 justify-center items-center space-x-1 flex-shrink-0
 //               rounded-lg border border-white absolute bottom-0 left-[192px]">
 //                 <div class="text-[#FC563D] font-inter text-[30px] font-semibold leading-normal">
 //                   Read more . . .
@@ -155,7 +166,6 @@ export default SlideShow;
 //           </div>
 //         ))}
 //       </div>
-
 
 //       <div className="Tap flex justify-center space-x-4 absolute bottom-2 left-1/2 transform -translate-x-1/2 py-2 ">
 //         {news.map((_, idx) => (
@@ -214,4 +224,3 @@ export default SlideShow;
 //     </>
 //   )
 // }
-
