e88d452afa7e1e838044b4f24b6711f059caca5e
Weerachat <Weerachat.kdw@kkumail.com>
2023-10-26 20:45:16 +0700

    [Update Logo image]

--- Project-AthleticsProgramManagement/src/components/login/components/loginPage.jsx
+++ Project-AthleticsProgramManagement/src/components/login/components/loginPage.jsx
@@ -1,63 +1,63 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
-import runningImg from './running.png'
-import GLogin from './googleLogin'
+import runningImg from '../././../../assets/images/runningLogo.svg';
+import GLogin from './googleLogin';
 // import runningImg from '../../../assets/images/'
 import './login.css'
 function LoginPage() {
   return (
     <>
-      <div className="h-calc flex items-center justify-center bg-blue-gradient overflow-hidden">
-          <img className='h-[80%]'
-          src={runningImg} alt="Web logo Athletics Program Management"/>
-          <div className='Blank'></div>
-          <section className='LoginMenu flex flex-col justify-center items-center w-[611px] absolute right-[0px] '>
-            <div className='UserLogin flex flex-col items-center'>
-              <label className='text-shadow text-[40px] font-[900]'>
-                User Login
-              </label>
-              <line className='w-[170px] h-[5px] bg-[#fff] mt-[18px]' />
-              <line className='w-[70px] h-[3px] bg-[#fff] mt-[13px]' />
-              <div className='Textfield'>
-                <div className='Name relative'>
-                  <div class="IconPerson w-[76px] h-[71px] bg-white rounded-full flex items-center justify-center absolute mt-[51px]">
-                    <svg width="41" height="47" viewBox="0 0 41 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M20.3915 23.3046C26.8276 23.3046 32.0438 18.0884 32.0438 11.6523C32.0438 5.21623 26.8276 0 20.3915 0C13.9555 0 8.73923 5.21623 8.73923 11.6523C8.73923 18.0884 13.9555 23.3046 20.3915 23.3046ZM28.5481 26.2177H27.0279C25.0069 27.1462 22.7584 27.6742 20.3915 27.6742C18.0247 27.6742 15.7852 27.1462 13.7552 26.2177H12.2349C5.48022 26.2177 0 31.6979 0 38.4526V42.2396C0 44.652 1.95722 46.6092 4.36961 46.6092H36.4134C38.8258 46.6092 40.7831 44.652 40.7831 42.2396V38.4526C40.7831 31.6979 35.3028 26.2177 28.5481 26.2177Z" fill="black" />
-                    </svg>
-                  </div>
-                  <div className='Input flex flex-col ml-[60px]'>
-                    <label className='ml-[30px] mb-[1px] mt-[40px] text-white'>Name - Last name</label>
-                    <input
-                      class="w-[345px] h-[40px] bg-white text-gray-500 italic placeholder-gray-500 pl-[30px] rounded-md"
-                      type="text"
-                      placeholder="Your name ..."
-                    />
-                  </div>
-
+      <div className=" h-screen-nav flex items-center justify-center bg-blue-gradient overflow-hidden relative">
+        <img className='h-full absolute top-0 left-0'
+          src={runningImg} alt="Web logo Athletics Program Management" />
+        <div className='Blank'></div>
+        <section className='LoginMenu flex flex-col justify-center items-center w-[611px] absolute right-[0px] '>
+          <div className='UserLogin flex flex-col items-center'>
+            <label className='text-shadow text-[40px] font-[900]'>
+              User Login
+            </label>
+            <line className='w-[170px] h-[5px] bg-[#fff] mt-[18px]' />
+            <line className='w-[70px] h-[3px] bg-[#fff] mt-[13px]' />
+            <div className='Textfield'>
+              <div className='Name relative'>
+                <div class="IconPerson w-[76px] h-[71px] bg-white rounded-full flex items-center justify-center absolute mt-[51px]">
+                  <svg width="41" height="47" viewBox="0 0 41 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20.3915 23.3046C26.8276 23.3046 32.0438 18.0884 32.0438 11.6523C32.0438 5.21623 26.8276 0 20.3915 0C13.9555 0 8.73923 5.21623 8.73923 11.6523C8.73923 18.0884 13.9555 23.3046 20.3915 23.3046ZM28.5481 26.2177H27.0279C25.0069 27.1462 22.7584 27.6742 20.3915 27.6742C18.0247 27.6742 15.7852 27.1462 13.7552 26.2177H12.2349C5.48022 26.2177 0 31.6979 0 38.4526V42.2396C0 44.652 1.95722 46.6092 4.36961 46.6092H36.4134C38.8258 46.6092 40.7831 44.652 40.7831 42.2396V38.4526C40.7831 31.6979 35.3028 26.2177 28.5481 26.2177Z" fill="black" />
+                  </svg>
                 </div>
-                <div className='Password relative'>
-                  <div class="IconLock w-[76px] h-[71px] bg-white rounded-full flex items-center justify-center absolute mt-[37px]">
-                    <svg width="35" height="40"
-                      viewBox="0 0 35 40" fill="none"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path d="M30.859 17.281H29.0074V11.7264C29.0074 5.26146 23.746 0 17.281 0C10.8161 0 5.55462 5.26146 5.55462 11.7264V17.281H3.70308C1.65867 17.281 0 18.9397 0 20.9841V35.7964C0 37.8408 1.65867 39.4995 3.70308 39.4995H30.859C32.9034 39.4995 34.562 37.8408 34.562 35.7964V20.9841C34.562 18.9397 32.9034 17.281 30.859 17.281ZM22.8356 17.281H11.7264V11.7264C11.7264 8.66366 14.2183 6.17179 17.281 6.17179C20.3438 6.17179 22.8356 8.66366 22.8356 11.7264V17.281Z" fill="black" />
-                    </svg>
-                  </div>
-                  <div className='Input flex flex-col ml-[60px]'>
-                    <label className='ml-[30px] mb-[1px] mt-[26px] text-white'>Password</label>
-                    <input
-                      class="w-[345px] h-[40px] bg-white text-gray-500 italic placeholder-gray-500 pl-[30px] rounded-md"
-                      type="text"
-                      placeholder="Your password ..."
-                    />
-                  </div>
-
+                <div className='Input flex flex-col ml-[60px]'>
+                  <label className='ml-[30px] mb-[1px] mt-[40px] text-white'>Name - Last name</label>
+                  <input
+                    class="w-[345px] h-[40px] bg-white text-gray-500 italic placeholder-gray-500 pl-[30px] rounded-md"
+                    type="text"
+                    placeholder="Your name ..."
+                  />
                 </div>
+
               </div>
-              <line className='w-[470px] h-[3px] bg-[#fff] mt-[54px]' />
-              <br />
-              <GLogin/>
-              {/* <div className='GoogleLogin w-full flex justify-end flex-row'>
+              <div className='Password relative'>
+                <div class="IconLock w-[76px] h-[71px] bg-white rounded-full flex items-center justify-center absolute mt-[37px]">
+                  <svg width="35" height="40"
+                    viewBox="0 0 35 40" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M30.859 17.281H29.0074V11.7264C29.0074 5.26146 23.746 0 17.281 0C10.8161 0 5.55462 5.26146 5.55462 11.7264V17.281H3.70308C1.65867 17.281 0 18.9397 0 20.9841V35.7964C0 37.8408 1.65867 39.4995 3.70308 39.4995H30.859C32.9034 39.4995 34.562 37.8408 34.562 35.7964V20.9841C34.562 18.9397 32.9034 17.281 30.859 17.281ZM22.8356 17.281H11.7264V11.7264C11.7264 8.66366 14.2183 6.17179 17.281 6.17179C20.3438 6.17179 22.8356 8.66366 22.8356 11.7264V17.281Z" fill="black" />
+                  </svg>
+                </div>
+                <div className='Input flex flex-col ml-[60px]'>
+                  <label className='ml-[30px] mb-[1px] mt-[26px] text-white'>Password</label>
+                  <input
+                    class="w-[345px] h-[40px] bg-white text-gray-500 italic placeholder-gray-500 pl-[30px] rounded-md"
+                    type="text"
+                    placeholder="Your password ..."
+                  />
+                </div>
+
+              </div>
+            </div>
+            <line className='w-[470px] h-[3px] bg-[#fff] mt-[54px]' />
+            <br />
+            <GLogin />
+            {/* <div className='GoogleLogin w-full flex justify-end flex-row'>
                 <div className='Label flex flex-col items-end mt-[30px] mr-[10px] text-white text-shadow'>
                   <label>Login with</label>
                   <label>Google</label>
@@ -72,8 +72,8 @@ function LoginPage() {
                 </div>
               </div> */}
 
-            </div>
-          </section>
+          </div>
+        </section>
 
       </div>
     </>
